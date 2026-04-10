3f0f53f6ae94c826fbffcdd0b602f1f9b09b78c5
aldri <11231050@student.itk.ac.id>
2026-04-10 14:02:53 +0800

    final fe

--- client/src/types.ts
+++ client/src/types.ts
@@ -6,24 +6,20 @@ export type ProductType = {
   shortDescription: string;
   description: string;
   price: number;
-  sizes: string[];
-  colors: string[];
-  images: Record<string, string>;
+  image: string;
 };
 
 export type ProductsType = ProductType[];
 
 export type CartItemType = ProductType & {
   quantity: number;
-  selectedSize: string;
-  selectedColor: string;
 };
 
 export type CartItemsType = CartItemType[];
 
 export const shippingFormSchema = z.object({
   name: z.string().min(1, "Name is required!"),
-  email: z.email().min(1, "Email is required!"),
+  email: z.string().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format!").min(1, "Email is required!"),
   phone: z
     .string()
     .min(7, "Phone number must be between 7 and 10 digits!")
